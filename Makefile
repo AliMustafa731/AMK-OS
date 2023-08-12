e5e8f3f7aba83fd82287a0caeba5868ef6e71c0d
ALi Kamel <ali200454a@gmail.com>
2023-08-12 22:07:19 +0300

    Enabled line A20

--- Makefile
+++ Makefile
@@ -4,20 +4,20 @@ MAKEFLAGS += --no-builtin-rules
 qemu = D:\Files\Programs\qemu\qemu-system-i386
 dd = Tools\dd
 src = src
-dist = build
 asm = Tools\nasm\nasm
 
-deps=$(wildcard $(src)/*.d)
+ASM_SRC = $(wildcard $(src)/*.asm)
+C_SRC = $(wildcard $(src)/*.c)
+OBJS = $(ASM_SRC:.asm=.o) $(C_SRC:.c=.o)
+DEPS = $(OBJS:.o=.d)
 
-run: build
+run: boot.bin
 	$(qemu) -accel hax -hda amkos.img
 
--include $(deps)
-
-build: boot.bin
+-include $(DEPS)
 
 boot.bin: $(src)\boot.asm
-	$(asm) -f bin -i$(src)/ -MD $(src)\boot.d $(src)\boot.asm -o boot.bin
+	$(asm) -f bin -i$(src)/ -MD $(<:.asm=.d) $< -o $@
 	$(dd) if=boot.bin of=amkos.img seek=0
 
 clean:
--- src/boot.asm
+++ src/boot.asm
@@ -23,9 +23,9 @@
 times 510 - ($-$$) db 0
 dw 0xAA55
 
-;----------------------------------------------
+;-----------------------------------------------
 ;    End of sector 1 & start of protected mode
-;----------------------------------------------
+;-----------------------------------------------
 
 %include "gdt_32.inc"
 %include "print_32.inc"
@@ -33,16 +33,16 @@ dw 0xAA55
 [bits 16]
 
 switch_to_pm:
-    cli ; disable interrupts
-    lgdt [gdt_descriptor] ; load the GDT descriptor
-    mov eax, cr0
-    or eax, 0x1 ; set bit 1 in cr0
-    mov cr0, eax
-    jmp CODE_SEG:init_pm ; far jump
+    cli		; disable interrupts
+    lgdt	[gdt_descriptor] ; load the GDT descriptor
+    mov		eax, cr0
+    or		eax, 0x1 ; set bit 1 in cr0
+    mov		cr0, eax
+    jmp		CODE_SEG:init_pm ; far jump
 
 [bits 32]
 
-init_pm: ; we are now using 32-bit instructions
+init_pm:
     mov ax, DATA_SEG ;  update the segment registers
     mov ds, ax
     mov ss, ax
@@ -52,7 +52,50 @@ init_pm: ; we are now using 32-bit instructions
 
     mov ebp, 0x90000 ; update the stack right at the top of the free space
     mov esp, ebp
+
+	;----------------------------------
+	;   Enable Line A20
+	;----------------------------------
+
+	call	ctrl_wait_input_empty
+	mov		al, 0xAD  ; disable keyboard command
+	out		0x64, al
+
+	call	ctrl_wait_input_empty
+	mov		al, 0xD0  ; read output port command
+	out		0x64, al
+
+	call	ctrl_wait_output_full
+	in		al, 0x60
+	push	eax
+
+	call	ctrl_wait_input_empty
+	mov		al, 0xD1  ; write output port command
+	out		0x64, al
+
+	call	ctrl_wait_input_empty
+	pop		eax
+	or		al, 2  ; set bit 1 (eanble A20)
+	out		0x60, al
+
+	call	ctrl_wait_input_empty
+	mov		al, 0xAE  ; enable keyboard command
+	out		0x64, al
+	call	ctrl_wait_input_empty
+
 	jmp start_pm
+
+ctrl_wait_input_empty:
+    in		al, 0x64
+    test	al, 2
+    jnz		ctrl_wait_input_empty
+	ret
+ctrl_wait_output_full:
+    in		al, 0x64
+    test	al, 1
+    jz		ctrl_wait_output_full
+	ret
+
 
 start_pm:
 	call clear_screen
@@ -78,9 +121,9 @@ start_pm:
 	
 	jmp $
 
-msg_hello db "Hello World !!", 0
-msg_16bit_mode db "Started in 16-bit real mode", 0
-msg_32bit_mode db "Loaded 32-bit protected mode", 0
+msg_hello: db "Hello World !!", 0
+msg_16bit_mode: db "Started in 16-bit real mode", 0
+msg_32bit_mode: db "Loaded 32-bit protected mode", 0
 
 
 times 2048 db 0x00
